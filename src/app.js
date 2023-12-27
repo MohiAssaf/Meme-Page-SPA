cdc4a87a8080791396533dcca6d3b7e71de93630
Mohamed <mohamedabuassaf@gmail.com>
2023-12-27 16:44:51 +0200

    init login view

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
-import {render, page} from './lib.js'
+import { render, page } from './lib.js'
+import { allMems } from './views/allmemes.js';
 import { homeView } from './views/home.js';
+import { loginView } from './views/login.js';
 
 
 const main = document.querySelector('main') // where the views should be displayed
 
 page(decorateContext); // this func will always be executed on all routes
 page('/', homeView);
-page('/memes', ()=> console.log('memes'));
-page('/memes/:id', ()=> console.log('meme detail'));
-page('/edit/:id', ()=> console.log('edit'));
-page('/create', ()=> console.log('create meme'));
-page('/profile', ()=> console.log('profile'));
-page('/login', ()=> console.log('login'));
-page('/register', ()=> console.log('registe'));
+page('/login', loginView);
+page('/register', () => console.log('registe'));
+page('/memes', allMems);
+page('/memes/:id', () => console.log('meme detail'));
+page('/edit/:id', () => console.log('edit'));
+page('/create', () => console.log('create meme'));
+page('/profile', () => console.log('profile'));
 
 
 
 page.start(); // starting the application
 
-function decorateContext(ctx, next){ // instead od adding the view in the main in each one of the templates
+function decorateContext(ctx, next) { // instead od adding the view in the main in each one of the templates
     ctx.render = renderMain;
 
     next();
 }
 
 
-function renderMain(templateResult){
+function renderMain(templateResult) {
     render(templateResult, main)
 }
