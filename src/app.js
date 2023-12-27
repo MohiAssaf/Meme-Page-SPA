5183f5601f567ce46845908061ccc3ec10cf1027
Mohamed <mohamedabuassaf@gmail.com>
2023-12-27 16:28:15 +0200

    init home view

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import {render, page} from './lib.js'
+import { homeView } from './views/home.js';
 
 
-page('/', ()=> console.log('home'));
+const main = document.querySelector('main') // where the views should be displayed
+
+page(decorateContext); // this func will always be executed on all routes
+page('/', homeView);
 page('/memes', ()=> console.log('memes'));
 page('/memes/:id', ()=> console.log('meme detail'));
 page('/edit/:id', ()=> console.log('edit'));
@@ -13,3 +17,14 @@ page('/register', ()=> console.log('registe'));
 
 
 page.start(); // starting the application
+
+function decorateContext(ctx, next){ // instead od adding the view in the main in each one of the templates
+    ctx.render = renderMain;
+
+    next();
+}
+
+
+function renderMain(templateResult){
+    render(templateResult, main)
+}
