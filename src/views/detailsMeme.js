3b29d7b1e9f594c04f822366b6cf273142f62aa9
Mohamed <mohamedabuassaf@gmail.com>
2024-01-26 18:21:11 +0200

    adding the functionality for the details of specific meme

--- src/views/detailsMeme.js
+++ src/views/detailsMeme.js
@@ -1,7 +1,8 @@
 import { detailsMeme } from "../api/meme.js";
 import { html } from "../lib.js";
+import { getUserData } from "../util.js";
 
-const detailsTemplate = (meme) => html`
+const detailsTemplate = (meme, isCreator) => html`
 <section id="meme-details">
 <h1>Meme Title: ${meme.title} </h1>
 <div class="meme-details">
@@ -12,9 +13,9 @@ const detailsTemplate = (meme) => html`
         <h2>Meme Description</h2>
         <p>${meme.description}</p>
 
-        ${
-            meme ? html `        
-            <a class="button warning" href="#">Edit</a>
+        ${// if its the cretor show the two buttons else dont
+            isCreator ? html `        
+            <a class="button warning" href="/edit/${meme._id}">Edit</a>
             <button class="button danger">Delete</button>`
             : ''
          }
@@ -25,6 +26,9 @@ const detailsTemplate = (meme) => html`
 
 export async function detailsView(ctx){
     const meme = await detailsMeme(ctx.params.id)
+    const user = getUserData();
 
-    ctx.render(detailsTemplate(meme))
+    const isCreator = user?.id == meme._ownerId // checks if its owner or not
+    
+    ctx.render(detailsTemplate(meme, isCreator))
 }
