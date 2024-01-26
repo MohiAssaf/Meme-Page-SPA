aaad1d6caeb525498e10b62990cdbfdf430cc59d
Mohamed <mohamedabuassaf@gmail.com>
2024-01-26 17:48:36 +0200

    adding the functionality for the details of specific meme

--- src/views/detailsMeme.js
+++ src/views/detailsMeme.js
@@ -1,30 +1,30 @@
+import { detailsMeme } from "../api/meme.js";
 import { html } from "../lib.js";
 
-const detailsTemplate = () => html`
+const detailsTemplate = (meme) => html`
 <section id="meme-details">
-<h1>Meme Title: Bad code can present some problems
-
-</h1>
+<h1>Meme Title: ${meme.title} </h1>
 <div class="meme-details">
     <div class="meme-img">
-        <img alt="meme-alt" src="/images/3.png">
+        <img alt="meme-alt" src=${meme.imageUrl}>
     </div>
     <div class="meme-description">
         <h2>Meme Description</h2>
-        <p>
-            Being a programmer is a fun job. And many funny incidents occur throughout a
-            programmer’s career.
-            Here are a few jokes that can be relatable to you as a programmer.
-        </p>
+        <p>${meme.description}</p>
 
-        <!-- Buttons Edit/Delete should be displayed only for creator of this meme  -->
-        <a class="button warning" href="#">Edit</a>
-        <button class="button danger">Delete</button>
+        ${
+            meme ? html `        
+            <a class="button warning" href="#">Edit</a>
+            <button class="button danger">Delete</button>`
+            : ''
+         }
         
     </div>
 </div>
 </section>`
 
-export function detailsView(ctx){
-    ctx.render(detailsTemplate())
+export async function detailsView(ctx){
+    const meme = await detailsMeme(ctx.params.id)
+
+    ctx.render(detailsTemplate(meme))
 }
