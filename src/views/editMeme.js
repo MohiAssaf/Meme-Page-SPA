21558daecfc47ee8501bd721b70b01c797bf17de
Mohamed <mohamedabuassaf@gmail.com>
2024-01-27 22:14:27 +0200

    adding the functionality for the edit meme view

--- src/views/editMeme.js
+++ src/views/editMeme.js
@@ -1,34 +1,31 @@
+import { detailsMeme } from "../api/meme.js";
 import { html } from "../lib.js";
 
-const editMemeTemplate = (onSubmit) => html`
+const editMemeTemplate = (meme, onSubmit) => html`
 <section id="edit-meme">
 <form @submit=${onSubmit} id="edit-form">
     <h1>Edit Meme</h1>
     <div class="container">
         <label for="title">Title</label>
-        <input id="title" type="text" placeholder="Enter Title" name="title">
+        <input id="title" type="text" placeholder="Enter Title" name="title" .value=${meme.title}>
         <label for="description">Description</label>
-        <textarea id="description" placeholder="Enter Description" name="description">
-                Programming is often touted as a smart and lucrative career path.
-                It's a job that (sometimes) offers flexibility and great benefits.
-                But it's far from sunshine and Nyan Cat rainbows. The hours are long.
-                The mistakes are frustrating. And your eyesight is almost guaranteed to suffer.
-                These memes cover most of the frustration (and funny moments) of programming.
-                At least we can laugh through the pain. 
-            </textarea>
+        <textarea id="description" placeholder="Enter Description" name="description" .value=${meme.description}></textarea>
         <label for="imageUrl">Image Url</label>
-        <input id="imageUrl" type="text" placeholder="Enter Meme ImageUrl" name="imageUrl">
+        <input id="imageUrl" type="text" placeholder="Enter Meme ImageUrl" name="imageUrl" .value=${meme.imageUrl}>
         <input type="submit" class="registerbtn button" value="Edit Meme">
     </div>
 </form>
 </section>`
 
-export function editMemeView(ctx){
-    ctx.render(editMemeTemplate(onSubmit))
+export async function editMemeView(ctx){
+    const meme = await detailsMeme(ctx.params.id)
+    ctx.render(editMemeTemplate(meme, onSubmit))
 
     async function onSubmit(e){
         e.preventDefault();
         const formData = new FormData(e.target)
+
+
 
 
     }
