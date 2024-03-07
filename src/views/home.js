8de80ab3535f0f0ca29fbff8e9635b9350945fbb
Mohamed <mohamedabuassaf@gmail.com>
2024-03-07 16:35:23 +0200

    new welcoming page

--- src/views/home.js
+++ src/views/home.js
@@ -3,13 +3,15 @@ import { getUserData } from '../util.js'
 
 const homeTemplate = () => html`
 <section id="welcome">
-<div id="welcome-container">
+
+<div class="welcome-container">
     <h1>Welcome To Meme Lounge</h1>
     <img src="/images/back.jpg" alt="meme">
     <h2>Login to see our memes right away!</h2>
-    <div id="button-div">
-        <a href="/login" class="button">Login</a>
-        <a href="/register" class="button">Register</a>
+
+    <div class="button-div">
+        <a href="/login" class="button login-button">Login</a>
+        <a href="/register" class="button register-button">Register</a>
     </div>
 </div>
 </section>`
